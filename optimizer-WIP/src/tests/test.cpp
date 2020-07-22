b6798d4d649651059eeb9a5d6953a47632801514
Tobias Gödderz <tobias@arangodb.com>
2020-07-22 13:29:24 +0200

    Added mpark/patterns library

--- optimizer-WIP/src/tests/test.cpp
+++ optimizer-WIP/src/tests/test.cpp
@@ -1,9 +1,22 @@
 #include "gtest/gtest.h"
 
+#include "mpark/patterns.hpp"
+
 #include <Logger/LogMacros.h>
+
+#include <string>
 
 namespace {
 
-TEST(FooTest, Bar) { LOG_DEVEL << "Hello, world."; }
+TEST(FooTest, Bar) {
+  using namespace std::string_literals;
+  auto const p = std::make_pair(42, "world"s);
 
+  using namespace mpark::patterns;
+  IDENTIFIERS(str);
+  LOG_DEVEL << "Hello, " << match(p)(pattern(ds(42, str)) = [](auto const& str) {
+    return str;
+  });
 }
+
+}  // namespace
