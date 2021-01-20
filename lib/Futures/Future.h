6437a45a13573e3fe54cd87a9e2ef3422d8ee521
maierlars <lars@arangodb.com>
2021-01-20 17:32:16 +0100

    Configured futures for performance test.

--- lib/Futures/Future.h
+++ lib/Futures/Future.h
@@ -97,9 +97,9 @@ struct mellon::tag_trait<arangodb::futures::arangodb_tag> {
     }
   };
 
-  static constexpr auto finally_prealloc_size = 32;
-  static constexpr auto small_value_size = 64;
-  static constexpr bool disable_temporaries = false;
+  static constexpr auto finally_prealloc_size = 0;
+  static constexpr auto small_value_size = 0;
+  static constexpr bool disable_temporaries = true;
 
   template<typename T, template<typename> typename Fut>
   struct user_defined_additions;
